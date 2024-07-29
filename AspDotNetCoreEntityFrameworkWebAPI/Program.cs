cdc241f558f85462169c2544d44539fe48534efd
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2024-07-29 15:57:16 +0530

    Installed EF Core and setup Dbcontext

--- AspDotNetCoreEntityFrameworkWebAPI/Program.cs
+++ AspDotNetCoreEntityFrameworkWebAPI/Program.cs
@@ -1,6 +1,14 @@
+using AspDotNetCoreEntityFrameworkWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Connection String Dependency
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
