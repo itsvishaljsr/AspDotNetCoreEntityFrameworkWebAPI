cff1319271773a281a76111bece3d8e7c01f9407
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2024-07-30 16:50:45 +0530

    table creation and master data addition

---
+++ AspDotNetCoreEntityFrameworkWebAPI/Migrations/20240730110857_masterDataCurrency.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AspDotNetCoreEntityFrameworkWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class masterDataCurrency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "currencies",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Indian rupees", "INR" },
+                    { 2, "Dollar", "Dollar" },
+                    { 3, "Euro", "Euro" },
+                    { 4, "Dinar", "Dinar" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "currencies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "currencies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "currencies",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
--- AspDotNetCoreEntityFrameworkWebAPI/Program.cs
+++ AspDotNetCoreEntityFrameworkWebAPI/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+ 
 //Connection String Dependency
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
